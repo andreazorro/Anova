ecb6192c1c1b277ba62f9a553c8145fa1b643108
Andrea Zorro Aranda <dolly.zorro@udea.edu.co>
2020-03-15 22:18:39 -0500

    Add files via upload

--- Anova.m
+++ Anova.m
@@ -1,28 +1,19 @@
-function [sNet_a,sNet_f] = Anova(genes, regulators, expressiondata)
+function [sNet_a] = Anova(genes, regulators, expressiondata)
 
     warning ('off','all')
 
     expressiondata = expressiondata';
     
-    nr = size(regulators,1);
-    tfs = zeros(1,nr);
-
-    parfor i = 1:nr
-        try
-            tfs(1,i) = find(strcmp(genes,regulators{i}));
-        catch
-            continue
-        end
-    end
-
+    tfs = cellfun(@(x)find(strcmp(x,gene_names)),regulators,'UniformOutput',false);
+    tfs(logical(cellfun('isempty',tfs))) = {0};
+    tfs = cell2mat(tfs);
     tfs = tfs(tfs ~= 0);
 
     ngenes = size(expressiondata,1);
-    ntf = size(tfs,2);
+    ntf = size(tfs,1);
 
     n2_a=zeros(ngenes,ntf);
-    n2_f=zeros(ngenes,ntf); 
-
+  
     tfexpression = expressiondata(tfs,:);
 
     for i = 1:ngenes
@@ -37,54 +28,35 @@ function [sNet_a,sNet_f] = Anova(genes, regulators, expressiondata)
                 expredatap_1 = tfexpression(j,:); 
 
                 [p_a,SS_a] = anova2([expredatap_1;expredatap_2],1,'off');
-                [p_f,SS_f] = friedman([expredatap_1;expredatap_2],1,'off');
-
+                
                 if p_a(1,2) < 0.05 
                     np_a=SS_a{3,2}/SS_a{5,2};   %SSA/SStotal
                 else
                     np_a=0;
                 end
-                if p_f < 0.05 
-                    np_f=SS_f{2,2}/SS_f{4,2};   %SSA/SStotal
-                else 
-                    np_f=0;
-                end
-
+               
                 expredatan_1 = -expredatap_1;
                 expredatan_2 = expredatap_2;
 
                 [p_a,SS_a] = anova2([expredatan_1;expredatan_2],1,'off');
-                [p_f,SS_f] = friedman([expredatan_1;expredatan_2],1,'off')
-
+         
                 if p_a(1,2) < 0.05 
                     nn_a=SS_a{3,2}/SS_a{5,2};  %SSA/SStotal
                 else
                     nn_a=0;
                 end
-                if p_f < 0.05 
-                    nn_f=SS_f{2,2}/SS_f{4,2};  %SSA/SStotal
-                else 
-                    nn_f=0;
-                end
-
+               
                 if np_a>nn_a
                     n2_a(i,j)=np_a;
                 else
                     n2_a(i,j)=-nn_a;
-                end
-
-                if np_f>nn_f
-                    n2_f(i,j)=np_f;
-                else
-                    n2_f(i,j)=-nn_f;
                 end
             end
         end
     end
 
     n2_a=abs(n2_a);
-    n2_f=abs(n2_f);
-
+   
     Net_a = cell(nnz(n2_a),3);
     
     r=1;
@@ -103,26 +75,7 @@ function [sNet_a,sNet_f] = Anova(genes, regulators, expressiondata)
     end
 
     sNet_a = sortrows(Net_a,3,'descend');
-   
-    Net_f = cell(nnz(n2_f),3);
-    
-    r=1;
-
-    for i = 1:ntf
-        for j = 1:ngenes 
-            if n2_f(j,i) == 0 
-                continue
-            else
-                Net_f{r,1} = genes{tfs(i)};
-                Net_f{r,2} = genes{j};
-                Net_f{r,3} = n2_f(j,i);
-                r = r+1;
-            end
-        end
-    end
-
-    sNet_f = sortrows(Net_f,3,'descend');
-    
+      
 end
 
 
